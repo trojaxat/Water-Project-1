f87e2d0234a6002d8a3d6ec79b96c49325766afa
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2018-07-23 21:54:35 +0200

    xxxxx
    x

--- includes/register.php
+++ includes/register.php
@@ -3,18 +3,18 @@
 if (isset($_POST['register'])) {
 	include_once 'checklogin.php';
 	
-	$Name = mysqli_real_escape_string($conn, $_POST['Name']);
-	$Last = mysqli_real_escape_string($conn, $_POST['Last']);
-	$Username = mysqli_real_escape_string($conn, $_POST['Username']);
-	$Email = mysqli_real_escape_string($conn, $_POST['Email']);
+	$name = mysqli_real_escape_string($conn, $_POST['name']);
+	$last = mysqli_real_escape_string($conn, $_POST['last']);
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
 	$DOB L= mysqli_real_escape_string($conn, $_POST['DOB']);
-	$Gender = mysqli_real_escape_string($conn, $_POST['Gender']);
+	$gender = mysqli_real_escape_string($conn, $_POST['gender']);
 	
-if(empty($Name) || empty($Last) || empty($Username) || empty($Email) || empty($DOB) || empty($Gender)) {
+if(empty($name) || empty($last) || empty($username) || empty($email) || empty($DOB) || empty($gender)) {
 	header("Location: ../register.php")
 	exit();
 } else {
-	if (!preg_match("/^[a-zA-Z]*$/", $Name) || !preg_match("/^[a-zA-Z]*$/", $Last)) {	
+	if (!preg_match("/^[a-zA-Z]*$/", $name) || !preg_match("/^[a-zA-Z]*$/", $last)) {	
 		header("Location: ../register.php")
 		exit();
 	} else {
@@ -22,7 +22,7 @@ if(empty($Name) || empty($Last) || empty($Username) || empty($Email) || empty($D
 			header("Location: ../register.php?register=Email");
 			exit();
 		} else {
-			$sql = "SELECT * FROM web_members WHERE Username = '$Username'";
+			$sql = "SELECT * FROM web_members WHERE Username = '$username'";
 			$result = mysqli_query($conn, $sql);
 			$resultCheck = mysql_num_rows($result);
 			
@@ -31,7 +31,7 @@ if(empty($Name) || empty($Last) || empty($Username) || empty($Email) || empty($D
 				exit();
 			} else {
 				$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-				$sql = "INSERT INTO web_members (Name, Last, Username, Email, DOB, Gender) VALUES ('$Name', '$Last', '$Username', '$Email', '$DOB', '$Gender');";
+				$sql = "INSERT INTO web_members (name, last, username, email, DOB, gender) VALUES ('$name', '$last', '$username', '$email', '$DOB', '$gender');";
 				mysqli_query($conn, $sql);
 				header("Location: ../register.php?register=login_success");
 				exit();
